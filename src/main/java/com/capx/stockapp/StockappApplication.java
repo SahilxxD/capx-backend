708c4f4494d1bd1106d0c1b3f0e1dd9df9dbc8af
Sahil Khairnar <95856189+SahilxxD@users.noreply.github.com>
2025-01-08 20:09:52 +0530

    Update StockappApplication.java

--- src/main/java/com/capx/stockapp/StockappApplication.java
+++ src/main/java/com/capx/stockapp/StockappApplication.java
@@ -2,6 +2,7 @@ package com.capx.stockapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.capx.stockapp.services")
